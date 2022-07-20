34b16fdc34cd3aed846dc308fb9a7da120bd8e4b
Bayoura <kristin.seidelmann@gmx.de>
2022-07-20 18:35:22 +0200

    add handleChange functions, add addSkill and removeSkill functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import ContactInfo from "./components/ContactInfo";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
 import Preview from "./components/preview/Preview";
 
 class App extends React.Component {
@@ -15,30 +19,90 @@ class App extends React.Component {
         phone: "",
       },
       education: {
-        school: "",
-        city: "",
+        facility: "",
+        edCity: "",
         degree: "",
-        startDate: "",
-        endDate: "",
+        edFrom: "",
+        edTo: "",
       },
-      work: {
+      experience: {
         company: "",
         position: "",
         tasks: "",
-        city: "",
-        startDate: "",
-        endDate: "",
+        expCity: "",
+        expFrom: "",
+        expTo: "",
       },
-      skills: [],
+      skills: {
+        skill: "",
+        skillList: [],
+      },
     };
-    this.handlePersonalChange = this.handlePersonalChange.bind(this);
+    this.handleContactChange = this.handleContactChange.bind(this);
+    this.handleEducationChange = this.handleEducationChange.bind(this);
+    this.handleExperienceChange = this.handleExperienceChange.bind(this);
+    this.handleSkillChange = this.handleSkillChange.bind(this);
+    this.addSkill = this.addSkill.bind(this);
+    this.removeSkill = this.removeSkill.bind(this);
   }
 
-  handlePersonalChange(e) {
+  handleContactChange(e) {
     const { name, value } = e.target;
-    const { ...personalData } = this.state.userInfo;
-    personalData[name] = value;
-    this.setState({ userInfo: personalData });
+    const { ...contactData } = this.state.userInfo;
+    contactData[name] = value;
+    this.setState({ userInfo: contactData });
+  }
+
+  handleEducationChange(e) {
+    const { name, value } = e.target;
+    const { ...educationData } = this.state.education;
+    educationData[name] = value;
+    this.setState({ education: educationData });
+  }
+
+  handleExperienceChange(e) {
+    const { name, value } = e.target;
+    const { ...experienceData } = this.state.experience;
+    experienceData[name] = value;
+    this.setState({ experience: experienceData });
+  }
+
+  handleSkillChange(e) {
+    const [...list] = this.state.skills.skillList;
+    this.setState({
+      skills: {
+        skill: e.target.value,
+        skillList: list,
+      },
+    });
+  }
+
+  addSkill() {
+    const newSkill = this.state.skills.skill;
+    if (newSkill === "") return;
+    this.setState({
+      skills: {
+        skillList: this.state.skills.skillList.concat({
+          title: newSkill,
+          id: uuidv4(),
+        }),
+        skill: "",
+      },
+    });
+  }
+
+  removeSkill(id) {
+    const newList = [...this.state.skills.skillList];
+    newList.splice(
+      newList.findIndex((skill) => skill.id === id),
+      1
+    );
+    this.setState({
+      skills: {
+        skill: this.state.skills.skill,
+        skillList: newList,
+      },
+    });
   }
 
   render() {
@@ -49,14 +113,17 @@ class App extends React.Component {
         </header>
         <main>
           <form>
-            <ContactInfo handleChange={this.handlePersonalChange} />
+            <ContactInfo handleContactChange={this.handleContactChange} />
+            <Education handleEducationChange={this.handleEducationChange} />
+            <Experience handleExperienceChange={this.handleExperienceChange} />
+            <Skills
+              skills={this.state.skills}
+              handleSkillChange={this.handleSkillChange}
+              addSkill={this.addSkill}
+              removeSkill={this.removeSkill}
+            />
           </form>
-          <Preview
-            fName={this.state.userInfo.fName}
-            lName={this.state.userInfo.lName}
-            mail={this.state.userInfo.mail}
-            phone={this.state.userInfo.phone}
-          />
+          <Preview cv={this.state} />
         </main>
       </div>
     );
