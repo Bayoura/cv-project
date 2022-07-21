b46ee6b9b544cbeae9f96c9c481f8031cb8dee6f
Bayoura <kristin.seidelmann@gmx.de>
2022-07-21 19:24:56 +0200

    add feature: if user has no work experience it won't show up in the preview

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -6,11 +6,31 @@ import SkillList from "./SkillList";
 
 class Preview extends React.Component {
   render() {
+    // if user has no work experience, the section won't show up in the preview
+    let notEmpty = false;
+    this.props.cv.experienceList.some((experience) => {
+      for (let property in experience) {
+        if (experience[property] !== "") notEmpty = true;
+      }
+      return notEmpty;
+    });
+
+    // for (let property in this.props.cv.experienceList) {
+    //   if (this.props.cv.experienceList[property] !== "") notEmpty = true;
+    // }
+
+    let experience;
+    if (notEmpty) {
+      experience = <Experience experienceList={this.props.cv.experienceList} />;
+    } else {
+      experience = <></>;
+    }
+
     return (
       <div>
         <ContactInfo userInfo={this.props.cv.userInfo} />
-        <Education education={this.props.cv.education} />
-        <Experience experience={this.props.cv.experience} />
+        <Education educationList={this.props.cv.educationList} />
+        {experience}
         <SkillList skillList={this.props.cv.skills.skillList} />
       </div>
     );
